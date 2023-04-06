ff9912c0e4df4ca03a33ae143eedd745cf27c52b
LongNguyen1101 <longnguyenhoang1101.work@gmail.com>
2023-04-06 23:46:09 +0700

    Create server and client project

---
+++ TCPChat/TCPChat/Server/ServerForm.Designer.cs
@@ -0,0 +1,90 @@
+﻿namespace Server
+{
+    partial class ServerForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.rtxtShow = new System.Windows.Forms.RichTextBox();
+            this.dgvClient = new System.Windows.Forms.DataGridView();
+            this.btnRemove = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvClient)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // rtxtShow
+            // 
+            this.rtxtShow.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rtxtShow.Location = new System.Drawing.Point(327, 38);
+            this.rtxtShow.Name = "rtxtShow";
+            this.rtxtShow.Size = new System.Drawing.Size(1114, 612);
+            this.rtxtShow.TabIndex = 0;
+            this.rtxtShow.Text = "";
+            // 
+            // dgvClient
+            // 
+            this.dgvClient.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvClient.Location = new System.Drawing.Point(12, 38);
+            this.dgvClient.Name = "dgvClient";
+            this.dgvClient.RowHeadersWidth = 62;
+            this.dgvClient.RowTemplate.Height = 28;
+            this.dgvClient.Size = new System.Drawing.Size(295, 612);
+            this.dgvClient.TabIndex = 1;
+            // 
+            // btnRemove
+            // 
+            this.btnRemove.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnRemove.Location = new System.Drawing.Point(12, 683);
+            this.btnRemove.Name = "btnRemove";
+            this.btnRemove.Size = new System.Drawing.Size(295, 75);
+            this.btnRemove.TabIndex = 2;
+            this.btnRemove.Text = "Remove";
+            this.btnRemove.UseVisualStyleBackColor = true;
+            this.btnRemove.Click += new System.EventHandler(this.btnRemove_Click);
+            // 
+            // ServerForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1500, 787);
+            this.Controls.Add(this.btnRemove);
+            this.Controls.Add(this.dgvClient);
+            this.Controls.Add(this.rtxtShow);
+            this.Name = "ServerForm";
+            this.Text = "SERVER";
+            this.Load += new System.EventHandler(this.ServerForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvClient)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox rtxtShow;
+        private System.Windows.Forms.DataGridView dgvClient;
+        private System.Windows.Forms.Button btnRemove;
+    }
+}
+
---
+++ TCPChat/TCPChat/Server/ServerForm.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Server
+{
+    public partial class ServerForm : Form
+    {
+        public ServerForm()
+        {
+            InitializeComponent();
+        }
+        TcpListener server;
+        Dictionary<string, TcpClient> clients;
+        void Connect()
+        {
+            try
+            {
+                server = new TcpListener(IPAddress.Parse("127.0.0.1"), 1234 );
+                server.Start();
+                Task.Run(() =>
+                {
+                    TcpClient client = server.AcceptTcpClient();
+                    clients = new Dictionary<string, TcpClient>();
+                    NetworkStream stream = client.GetStream();
+                    try
+                    {
+                        StreamReader reader = new StreamReader(stream);
+                        string name = reader.ReadLine();
+                        clients.Add(name, client);
+                        rtxtShow.Text += (name + " has joined the chat.\n");
+                    }
+                    catch (IOException)
+                    {
+                        // Client has disconnected
+                        client.Close();
+                        return;
+                    }
+
+                    while (true)
+                    {
+                        try
+                        {
+                            StreamReader reader = new StreamReader(stream);
+                            string message = reader.ReadLine();
+
+                            if (message != null)
+                            {
+                                
+                                // Parse the message to extract the recipient and content
+                                string[] parts = message.Split(new char[] { ':' }, 2);
+                                string recipient = parts[0];
+                                string content = parts[1];
+                                rtxtShow.Text += ($"{recipient} : {content}");
+
+                                // Send the message to the recipient
+                                if (clients.ContainsKey(recipient))
+                                {
+                                    TcpClient recipientClient = clients[recipient];
+                                    StreamWriter writer = new StreamWriter(recipientClient.GetStream());
+                                    writer.WriteLine(content);
+                                    writer.Flush();
+                                }
+                                else
+                                {
+                                    rtxtShow.Text += ("Error: " + recipient + " not found.\n");
+                                }
+                            }
+                        }
+                        catch (IOException)
+                        {
+                            // Client has disconnected
+                            break;
+                        }
+                    }
+
+                    // Remove the client from the dictionary
+                    foreach (string name in clients.Keys)
+                    {
+                        if (clients[name] == client)
+                        {
+                            clients.Remove(name);
+                            rtxtShow.Text += (name + " has left the chat.\n");
+                            break;
+                        }
+                    }
+
+                    client.Close();
+                });
+            }
+            catch(Exception ex)
+            {
+
+            }
+        }
+        private void ServerForm_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnRemove_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
